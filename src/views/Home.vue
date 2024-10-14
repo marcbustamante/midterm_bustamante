39319c5455e722ad5983d439e5b2ed4c0d16b9a0
marcbustamante <marcbustamante570@gmail.com>
2024-10-14 16:42:19 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <h1>Welcome</h1>
+      <nav>
+        <router-link to="/home">Home</router-link> |
+        <router-link to="/products">Products</router-link>
+      </nav>
+      <p>Your Name: Marc Leobenz Bustamnate</p>
+      <p>Program: BSIT</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Home',
+  }
+  </script>
+  
+  <style scoped>
+  div {
+    font-family: 'Courier New', Courier, monospace;
+  }
+  </style>
